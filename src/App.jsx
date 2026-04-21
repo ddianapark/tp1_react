71d0760cee124e686fdf812c560106782b55234b
DIANA IRENE PARK <49377760@ort>
2026-04-21 09:52:52 -0300

    implementacion de useEffect y localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,35 @@ import './App.css'
 import Form from './components/Form.jsx'
 import Lista from './components/Lista.jsx'
 import Subtitulo from './components/Subtitulo.jsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const initialCitas = [
   { id: 0, nombre: "Nina", propietario: "Martin", fecha: "2021-08-05", hora: "08:20", sintomas: "Le duele la pierna" },
-  { id: 1, nombre: "Sifon", propietario: "Flecha", fecha: "2023-08-05", hora: "09:24", sintomas: "Duerme mucho" },
-  { id: 2, nombre: "Floki", propietario: "Ari", fecha: "2023-08-05", hora: "16:15", sintomas: "No está comiendo" }
+  { id: 1, nombre: "Sifon", propietario: "Flecha", fecha: "2023-08-05", hora: "09:24", sintomas: "Duerme mucho" }
 ]
 
 function App() {
-  const [citas, setCitas] = useState(initialCitas);
-  const [nextId, setNextId] = useState(4);
+  const [citas, setCitas] = useState(citas => {
+    const citasGuardadas = localStorage.getItem('citas');
+    return citasGuardadas ? JSON.parse(citasGuardadas) : initialCitas;
+  });
+  const [nextId, setNextId] = useState(0);
+  useEffect(() => {
+    localStorage.setItem('citas', JSON.stringify(citas));
+  }, [citas]);
 
   const addCita = (cita) => {
     const nuevaCita = { ...cita, id: nextId };
     setCitas([...citas, nuevaCita]);
     setNextId(nextId + 1);
+    
+    useEffect(() => {
+      const citasGuardadas = localStorage.getItem('citas');
+      if (citasGuardadas) {
+        setCitas(JSON.parse(citasGuardadas));
+      }
+      localStorage.setItem('citas', JSON.stringify(citasGuardadas));
+    }, [citas]);
   };
 
   const deleteCita = (id) => {
@@ -27,6 +40,13 @@ function App() {
       nuevasCitas.splice(index, 1);
       setCitas(nuevasCitas);
     }
+    useEffect(() => {
+      const citasGuardadas = localStorage.getItem('citas');
+      if (citasGuardadas) {
+        setCitas(JSON.parse(citasGuardadas));
+      }
+      localStorage.setItem('citas', JSON.stringify(citasGuardadas));
+    }, [citas]);
   };
 
   return (
@@ -35,7 +55,7 @@ function App() {
       <div class="container">
         <div class="row">
           <div class="one-half column">
-            <Subtitulo texto="Crear Mi Cita"/><Subtitulo texto="Crear Mi Cita"/>
+            <Subtitulo texto="Crear Mi Cita"/>
             <Form onAddCita={addCita} />
           </div>
           <div class="one-half column">
